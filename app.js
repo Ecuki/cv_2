e6d5ff08ed19849279a60a30c9dd9abe4c31abe9
Ecuki <31224521+Ecuki@users.noreply.github.com>
2020-03-28 23:48:57 +0100

    confige based on MERN Shopping LIST

--- app.js
+++ app.js
@@ -21,18 +21,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // DB Config
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 var howtos = express.Router();
 require("./server/howto.routes.js")(howtos);
-app.use("/api", howtos);
 
 // Connecting to the database
 mongoose.Promise = global.Promise;
@@ -60,9 +59,11 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(
       // path.resolve(process.env.PWD, "client", "build", "index.html")
       // path.resolve(__dirname, "./client/build", "index.html")
-      path.resolve(__dirname, "client", "build", "index.html")
+      // path.resolve(__dirname, "client", "build", "index.html")
+      path.join(__dirname, "client", "build", "index.html")
     );
   });
 }
+app.use("/api", howtos);
 
 module.exports = app;
