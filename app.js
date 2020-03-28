4f2a3c130ad764d41949a6529b18a69e18a82015
Ecuki <31224521+Ecuki@users.noreply.github.com>
2020-03-28 23:15:08 +0100

    confige based on MERN Shopping LIST

--- app.js
+++ app.js
@@ -21,15 +21,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // DB Config
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 var howtos = express.Router();
 require("./server/howto.routes.js")(howtos);
 app.use("/api", howtos);
