dee4ad311478152781ebc3c012d409e1fade8086
Ecuki <31224521+Ecuki@users.noreply.github.com>
2020-03-29 17:54:13 +0200

    add lazy loading to App component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Router, Switch, Route } from "react-router-dom";
 import { useAuth0 } from "../../Login/react-auth0-spa";
 import "./App.scss";
 import Header from "../Header";
 import Home from "../Home";
-import Projects from "../Projects";
-import Contact from "../Contact";
-import HowTo from "../HowTo";
-import Profile from "../Profile";
-import PrivateRoute from "../PrivateRoute";
 import history from "../../Login/utils/history";
 
+const Projects = React.lazy(() => import("../Projects"));
+const Contact = React.lazy(() => import("../Contact"));
+const HowTo = React.lazy(() => import("../HowTo"));
+const Profile = React.lazy(() => import("../Profile"));
+const PrivateRoute = React.lazy(() => import("../PrivateRoute"));
+
+// const Projects = React.lazy(() => import("../Projects"));
 function App() {
   const { loading } = useAuth0();
   const PATH = "/";
@@ -20,15 +22,17 @@ function App() {
   return (
     <div className="app">
       <Router history={history}>
-        <Header />
-        <Switch>
-          <Route path={`${PATH}`} exact component={Home} />
-          <Route path={`${PATH}projects`} component={Projects} />>
-          <Route path={`${PATH}contact`} component={Contact} />
-          <Route path={`${PATH}how-to`} component={HowTo} />
-          <PrivateRoute path={`${PATH}profile`} component={Profile} />
-          <Route path={`${PATH}`} render={() => <div>404</div>} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          <Switch>
+            <Route path={`${PATH}`} exact component={Home} />
+            <Route path={`${PATH}projects`} component={Projects} />
+            <Route path={`${PATH}contact`} component={Contact} />
+            <Route path={`${PATH}how-to`} component={HowTo} />
+            <PrivateRoute path={`${PATH}profile`} component={Profile} />
+            <Route path={`${PATH}`} render={() => <div>404</div>} />
+          </Switch>
+        </Suspense>
       </Router>
     </div>
   );
